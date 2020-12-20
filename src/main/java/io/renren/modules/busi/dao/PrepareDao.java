ccfde61e977d0cfe8f8847e0970a525d95d9c9c6
chenh <414019789@qq.com>
2020-12-20 11:49:16 +0800

    Middle

---
+++ src/main/java/io/renren/modules/busi/dao/PrepareDao.java
@@ -0,0 +1,17 @@
+package io.renren.modules.busi.dao;
+
+import io.renren.modules.busi.entity.PrepareEntity;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * WX报备表
+ * 
+ * @author liuh
+ * @email 18067435857@163.com
+ * @date 2020-12-20 11:18:56
+ */
+@Mapper
+public interface PrepareDao extends BaseMapper<PrepareEntity> {
+	
+}
---
+++ src/main/java/io/renren/modules/busi/service/PrepareService.java
@@ -0,0 +1,20 @@
+package io.renren.modules.busi.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import io.renren.common.utils.PageUtils;
+import io.renren.modules.busi.entity.PrepareEntity;
+
+import java.util.Map;
+
+/**
+ * WX报备表
+ *
+ * @author liuh
+ * @email 18067435857@163.com
+ * @date 2020-12-20 11:18:56
+ */
+public interface PrepareService extends IService<PrepareEntity> {
+
+    PageUtils queryPage(Map<String, Object> params);
+}
+
