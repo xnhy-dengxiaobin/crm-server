9bb0a764d4e13561f0725b05c416e33cfd229106
liuh <351095250@qq.com>
2020-12-23 22:10:38 +0800

    客户从报备到签约所经理的变更及状态变化

--- src/main/java/io/renren/modules/busi/bean/ActionEnum.java
+++ src/main/java/io/renren/modules/busi/bean/ActionEnum.java
@@ -4,16 +4,16 @@ package io.renren.modules.busi.bean;
  * 客户从报备到签约经历的不同的变更
  */
 public enum ActionEnum {
-    PREPARE(0, "报备"),
-    RE_PREPARE(10, "重新报备"),
-    EXPIRED(-30, "过期"),
-    VISITED(-10, "来访"),
-    MANNUL_AUDIT(-20, "人工确客"),
-    PROTECT_PERIOD_CHANGE(-20, "保护期变更"),
-    SOLICIT(-20, "认筹"),
-    TRANSCTION_VOID(-20, "交易作废"),
-    SUBSCRIBE(-20, "认购"),
-    SIGN(-20, "签约");
+    PREPARE(1, "报备"),
+    RE_PREPARE(5, "重新报备"),
+    EXPIRED(10, "过期"),
+    VISITED(20, "来访"),
+    MANNUL_AUDIT(30, "人工确客"),
+    PROTECT_PERIOD_CHANGE(35, "保护期变更"),
+    SOLICIT(40, "认筹"),
+    TRANSCTION_VOID(45, "交易作废"),
+    SUBSCRIBE(50, "认购"),
+    SIGN(60, "签约");
 
     private int code;
     private String label;
