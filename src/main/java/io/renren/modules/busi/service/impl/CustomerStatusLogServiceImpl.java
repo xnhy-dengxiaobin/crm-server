d819b83a50fd8f5ed8b02343a6904013fb7bf08f
liuh <351095250@qq.com>
2020-12-24 22:44:34 +0800

    状态变化流水 增加查询某个客户流水信息，返回list

--- src/main/java/io/renren/modules/busi/service/impl/CustomerStatusLogServiceImpl.java
+++ src/main/java/io/renren/modules/busi/service/impl/CustomerStatusLogServiceImpl.java
@@ -1,19 +1,18 @@
 package io.renren.modules.busi.service.impl;
 
-import org.springframework.stereotype.Service;
-
-import java.util.List;
-import java.util.Map;
-
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.metadata.IPage;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import io.renren.common.utils.PageUtils;
+import io.renren.common.utils.ParamResolvor;
 import io.renren.common.utils.Query;
-
 import io.renren.modules.busi.dao.CustomerStatusLogDao;
 import io.renren.modules.busi.entity.CustomerStatusLogEntity;
 import io.renren.modules.busi.service.CustomerStatusLogService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 
 @Service("customerStatusLogService")
@@ -31,6 +30,13 @@ public class CustomerStatusLogServiceImpl extends ServiceImpl<CustomerStatusLogD
 
     @Override
     public List<CustomerStatusLogEntity> queryList(Map<String, Object> params) {
-        return getBaseMapper().selectByMap(params);
+        Integer prepareId = ParamResolvor.getInt(params, "prepareId");
+        Integer customerId = ParamResolvor.getIntAsDefault(params, "customerId", 0);
+        return getBaseMapper().selectList(new QueryWrapper<CustomerStatusLogEntity>()
+                .eq("prepare_id", prepareId)
+                .or()
+                .eq(customerId > 0, "customer_id", customerId)
+        );
+
     }
 }
